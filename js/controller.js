0ebdd7dcb0b58f97926f0bb13631d73f41eb1360
Sarat Kumar K <saratsivam@gmail.com>
2015-10-19 19:00:07 +0530

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,41 +1,49 @@
+//$('.feed-carousel-shelf').draggable({axis: "x"});
 
+$('.gate-cart .gate').draggable({revert:true});
 
+$('#holder').droppable({	
+	drop:function(event,ui){				
+		var $dropped = ui.draggable;			
+		var p0 = $(this).offset();
+		var p =$dropped.offset();				
+		dl.createComponent({type:$dropped.attr('gatetype'),name:$dropped.attr('name'),x:(p.left-p0.left),y:(p.top-p0.top)});
+	}
+})
 
-$('.create-gate').click(function(){	
-	dl.createComponent({type:'GATE',name:$(this).attr('gatetype'),x:0,y:0});	
-});
-$('.bulb').click(function(){
-	dl.createComponent({type:'BULB',x:0,y:0});	
-});
-$('.switch').click(function(){	
-	dl.createComponent({type:'SWITCH',x:0,y:0});	
-});
-$('.wire').click(function(){
+$(function(){
 	dl.setMode(dl.modes.WIREMODE);
 });
 $('#simspeed').change(function(){	
 	dl.setSimSpeed(1000-$(this).val())
 });
 
+$('.stop').click(function(){
+	dl.stopSim();
+	$(this).hide();$('.run').show();
+});
+$('.run').click(function(){
+	dl.startSim();
+	$(this).hide();$('.stop').show();
+});
 
+$('.saveas').click(function(){
+	 document.location = 'data:Application/octet-stream,' + encodeURIComponent(dl.getCode());
+});
 
+$('.open').click(function(){
+	$('#fileopen').click();
+});
+$('#fileopen').change(function(){
+  var fileToLoad = this.files[0];	
+  var fileReader = new FileReader();
+  fileReader.onload = function(fileLoadedEvent) {
+    var textFromFileLoaded = fileLoadedEvent.target.result;   
+	 eval(textFromFileLoaded);
+  };
+  fileReader.readAsText(fileToLoad, "UTF-8");
+});
 
-
-$('.stop').click(dl.stopSim);
-$('.run').click(dl.startSim);
-
-
-
-var writeComponents = function(){
-	var str='';
-	$.each(components,function(i,c){
-		str+='\n'+dl.write(c);
-	});
-	console.log(str);
-};
-
-
-
-
-
-
+$('.example').click(function(e){	
+	examples[this.id]();
+});
