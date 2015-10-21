dcf2a42b0479af0b64026d14ae01364509d941b2
Sarat Kumar K <saratsivam@gmail.com>
2015-10-21 20:16:39 +0530

    Update logicgates-core.js

--- js/logicgates-core.js
+++ js/logicgates-core.js
@@ -1,7 +1,7 @@
 (function(){
 
 	var util={
-		updateAllWiresAtOutput:function(output, state){		
+		opdateOutputWires:function(output, state){		
 			for(var i in output){
 				output[i].state = state;
 			}
@@ -9,97 +9,103 @@
 		inherit:function(child, parent){
 			child.prototype = Object.create(parent.prototype);
 			child.prototype.constructor = child;
-		}
-	};
-	
-	
-	var WIRE = function(){		
-		this.state=false;		
+		},
+		noop:function(){
+		},
 	};	
 	
-	var Component = function(){		
+	// core objects....................................
+	var WIRE = function(){		
+		this.state=false;		
+	};
+
+	var Component = function(name){
+		this.name=name;
 		this.pins={};//each input holds array of wires		
 		this.state=false;
 		this.link = function(wire, pinId){			
 			if(!this.pins[pinId]){this.pins[pinId]=[]};
 			this.pins[pinId].push(wire);			
-		}
+		};
+		this.click = function(){};
 	};	
 	
+	//extended objects ...............................
+	
+	
 	var SWITCH = function(){
-		Component.call(this);
-		this.toggle=function(){
+		Component.call(this,'SWITCH');
+		this.click=function(){
 			this.state=!this.state;return this;
 		}
 		this.update = function(){
-			util.updateAllWiresAtOutput(this.pins[0], this.state);			
+			util.opdateOutputWires(this.pins[0], this.state);
+			(this.afterUpdate||util.noop)();//should be overriden for ui
 		};
 	};
 	util.inherit(SWITCH, Component);
 	
+	
+	var PULSER = function(period){
+		Component.call(this,'PULSER');		
+		this.period = 5;		
+		var counter = 0;		
+		this.update = function(){	
+			counter++;					
+			if(counter >= this.period){				
+				counter=0;
+				this.state=!this.state;				
+			}	
+			util.opdateOutputWires(this.pins[0], this.state);
+			(this.afterUpdate||util.noop)();//should be overriden for ui
+		};	
+	}
+	util.inherit(PULSER, Component);
+	
+	
 	var BULB = function(){
-		Component.call(this);
+		Component.call(this,'BULB');
 		this.update = function(){
 			this.state = this.pins[0][0].state;
+			(this.afterUpdate||util.noop)();//should be overriden for ui
 		};
 	};
 	util.inherit(BULB, Component);	
 	
-	var OR = function(){
-		Component.call(this);
-		this.update = function(){			
+	var GATE = function(name){
+		Component.call(this,name);
+		this.update = updateMethods[name];
+	}
+	util.inherit(GATE, Component);	
+	
+	
+	var updateMethods={
+		'OR':function(){			
 			this.state=this.pins[0][0].state||this.pins[1][0].state;					
-			util.updateAllWiresAtOutput(this.pins[2], this.state);		
+			util.opdateOutputWires(this.pins[2], this.state);		
 			return this;
-		};
-	};	
-	util.inherit(OR, Component);	
-	
-	var AND = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'AND':function(){
 			this.state=this.pins[0][0].state&&this.pins[1][0].state;					
-			util.updateAllWiresAtOutput(this.pins[2], this.state);			
+			util.opdateOutputWires(this.pins[2], this.state);			
 			return this;
-		};
-	};		
-	util.inherit(AND, Component);
-	
-	var NAND = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'NAND':function(){
 			this.state=!(this.pins[0][0].state&&this.pins[1][0].state);					
-			util.updateAllWiresAtOutput(this.pins[2], this.state);		
+			util.opdateOutputWires(this.pins[2], this.state);		
 			return this;
-		};	
-	};
-	util.inherit(NAND, Component);
-	
-	
-	var NOR = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'NOR':function(){
 			this.state=!(this.pins[0][0].state||this.pins[1][0].state);					
-			util.updateAllWiresAtOutput(this.pins[2], this.state);		
+			util.opdateOutputWires(this.pins[2], this.state);		
 			return this;
-		};
-	};	
-	util.inherit(NOR, Component);	
-	
-	var NOT = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'NOT':function(){
 			this.state = !this.pins[0][0].state;				
-			util.updateAllWiresAtOutput(this.pins[1], this.state);		
+			util.opdateOutputWires(this.pins[1], this.state);		
 			return this;
-		};
-	};
-	util.inherit(NOT, Component);
-	
-		
-	var RSFF = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'RSFF':function(){
 			var q1 = this.pins[2];//output node
 			var q2 = this.pins[3];//output node
 			//clk input is true if not connected
@@ -109,16 +115,11 @@
 			var q = this.state;//current state
 			if(clk){q=s||(q&&!r);}//Q(next) = S+QR'		
 			this.state=q;//updated state				
-			util.updateAllWiresAtOutput(q1, q);		
-			util.updateAllWiresAtOutput(q2, !q);		
+			util.opdateOutputWires(q1, q);		
+			util.opdateOutputWires(q2, !q);		
 			return this;
-		};	
-	};
-	util.inherit(RSFF, Component);
-	
-	var JKFF = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'JKFF':function(){
 			var q1 = this.pins[2];//output node
 			var q2 = this.pins[3];//output node
 			//clk input is true if not connected
@@ -128,16 +129,11 @@
 			var q = this.state;//current state
 			if(clk){q=(j&&!q)||(!k&&q);}//Q(next) = JQ' + K'Q	
 			this.state=q;//updated state				
-			util.updateAllWiresAtOutput(q1, q);		
-			util.updateAllWiresAtOutput(q2, !q);
+			util.opdateOutputWires(q1, q);		
+			util.opdateOutputWires(q2, !q);
 			return this;
-		};	
-	};
-	util.inherit(JKFF, Component);
-	
-	var DFF = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'DFF':function(){////Q(next) = D
 			var q1 = this.pins[2];//output node
 			var q2 = this.pins[3];//output node
 			//clk input is true if not connected
@@ -145,20 +141,18 @@
 			var d = this.pins[0][0].state;
 			//var r = this.pins[1][0].state;
 			var q = this.state;//current state
-			if(this.clk && !clk){q=d;}//Q(next) = D	on falling edge		
+			if(!this.clk && clk){//raising edge
+				this.metaState = d;
+			}else if(this.clk && !clk){//falling edge
+				q=this.metaState;		
+			}
 			this.state=q;//updated state
 			this.clk =clk;//updating clock
-			util.updateAllWiresAtOutput(q1, q);		
-			util.updateAllWiresAtOutput(q2, !q);	
+			util.opdateOutputWires(q1, q);		
+			util.opdateOutputWires(q2, !q);	
 			return this;
-		};	
-	};
-	util.inherit(DFF, Component);
-	
-	
-	var TFF = function(){
-		Component.call(this);
-		this.update = function(){
+		},
+		'TFF':function(){//Q(next) = TQ' + T'Q
 			var q1 = this.pins[2];//output node
 			var q2 = this.pins[3];//output node
 			//clk input is true if not connected
@@ -166,39 +160,34 @@
 			var t = this.pins[0][0].state;
 			//var r = this.pins[1][0].state;
 			var q = this.state;//current state
-			if(clk){q=(t&&!q)||(!t&&q);}//Q(next) = TQ' + T'Q
-			this.state=q;//updated state				
-			util.updateAllWiresAtOutput(q1, q);		
-			util.updateAllWiresAtOutput(q2, !q);	
+			if(!this.clk && clk){//raising edge
+				this.metaState = (t&&!q) || (!t&&q);
+			}else if(this.clk && !clk){//falling edge
+				q=this.metaState;		
+			}				
+			this.state=q;//updated state
+			this.clk =clk;//updating clock			
+			util.opdateOutputWires(q1, q);		
+			util.opdateOutputWires(q2, !q);	
 			return this;
-		};	
-	};
-	util.inherit(TFF, Component);
-	
-	
-	var gateMap = {
-		'AND':function(){return new AND()},
-		'OR':function(){return new OR()},
-		'NOT':function(){return new NOT()},
-		'NAND':function(){return new NAND()},
-		'NOR':function(){return new NOR()},
-		'RSFF':function(){return new RSFF()},
-		'JKFF':function(){return new JKFF()},
-		'DFF':function(){return new DFF()},
-		'TFF':function(){return new TFF()}
-	};
-	
-	var getGate = function(name){
-		return gateMap[name]();
+		},
+		'4017':function(){
+		
+		
+		}		
+	};		
+	var componentRegister={
+		'WIRE':WIRE,
+		'SWITCH':SWITCH,
+		'PULSER':PULSER,
+		'BULB':BULB,
+		'PULSER':PULSER,
+		'GATE':GATE
+		
 	};	
-	
 	window.dlcore = {
-		getGate:getGate,	
-		getWire:function(){return new WIRE()},
-		getSwitch:function(){return new SWITCH()},
-		getBulb:function(){return new BULB()}
+		createComponent:function(type, name){
+			return new componentRegister[type](name);
+		}		
 	};	
 }());
-
-
-
