d8e716f925b4b1b0e9528ce566734ffc9a82df45
Sarat Kumar K <saratsivam@gmail.com>
2015-10-21 20:16:11 +0530

    Update logicgates-contruct.js

--- js/logicgates-contruct.js
+++ js/logicgates-contruct.js
@@ -49,7 +49,7 @@
 	var createWire = function(pin1, pin2){
 		var wireConnection = r.connection(pin1, pin2, "#00f");
 		connections.push(wireConnection);				
-		var w = dlcore.getWire();
+		var w = dlcore.createComponent('WIRE');
 		w.ui = wireConnection;
 		//connecting 			
 		$.each([pin1,pin2],function(i,node){
@@ -64,51 +64,58 @@
 	};
 	
 	
-	var getDim = function(gateType,b,p){
+	var getPinConfig = function(gateType){	
+		var b={x:0,y:0,w:60,h:60,r:5};
+		var p={w:20,h:10,r:0};		
+		
+		var inputPin =[{x:-p.w,y:2*b.h/5,pinType:'in',pinId:0}];
+		var outputPin = [{x:b.w,y:2*b.h/5,pinType:'out',pinId:0}];			
 		var pins2x1 =  [
 			{x:-p.w,y:b.h/5,pinType:'in',pinId:0},
 			{x:-p.w,y:3*b.h/5,pinType:'in',pinId:1},
 			{x:b.w,y:2*b.h/5,pinType:'out',pinId:2}
-			];
+			];			
 		var pins1x1 = [
 				{x:-p.w,y:2*b.h/5,pinType:'in',pinId:0},
 				{x:b.w,y:2*b.h/5,pinType:'out',pinId:1}
-			];
+			];			
 		var pin2x2x1 = [
 			{x:-p.w,y:0.1*b.h/5,pinType:'in',pinId:0},
 			{x:-p.w,y:3.9*b.h/5,pinType:'in',pinId:1},
 			{x:b.w,y:0.1*b.h/5,pinType:'out',pinId:2},
 			{x:b.w,y:3.9*b.h/5,pinType:'out',pinId:3},
 			{x:-p.w,y:2*b.h/5,pinType:'clk',pinId:4}
-		];	
-			
-			
-		var pinset={
-		'AND':pins2x1,
-		'OR':pins2x1,
-		'NAND':pins2x1,
-		'NOR':pins2x1,
-		'NOT':pins1x1,
-		'RSFF':pin2x2x1,
-		'JKFF':pin2x2x1,
-		'DFF':pin2x2x1,
-		'TFF':pin2x2x1
-		};
-		
-		return pinset[gateType];		
+		];		
+		var pin10x3x1 = [
+			//{x:-p.w,y:0,pinType:'out',pinId:0}		
+		];		
+		var config={
+			'BULB':{pins:inputPin,p:p,b:b},
+			'SWITCH':{pins:outputPin,p:p,b:b},
+			'PULSER':{pins:outputPin,p:p,b:b},
+			'AND':{pins:pins2x1,p:p,b:b},
+			'OR':{pins:pins2x1,p:p,b:b},
+			'NAND':{pins:pins2x1,p:p,b:b},
+			'NOR':{pins:pins2x1,p:p,b:b},
+			'NOT':{pins:pins1x1,p:p,b:b},
+			'RSFF':{pins:pin2x2x1,p:p,b:b},
+			'JKFF':{pins:pin2x2x1,p:p,b:b},
+			'DFF':{pins:pin2x2x1,p:p,b:b},
+			'TFF':{pins:pin2x2x1,p:p,b:b},
+			'4017':{pins:pin10x3x1,p:{w:10,h:20,r:0},b:{x:0,y:0,w:120,h:60,r:5}}
+		};		
+		return config[gateType];		
 	};
 	
 	
-	
-	
-	var createGate = function(gatetype){	
-		var component = dlcore.getGate(gatetype);	
-		var color = Raphael.getColor();	
-		var b={x:0,y:0,w:60,h:60,r:5};
-		var p={w:20,h:10,r:0};		
-		var pinpositions =getDim(gatetype,b,p);		
-		var group=[];
-		$.each(pinpositions,function(i,pos){
+	var constructComponent = function(type, name){
+		var component = dlcore.createComponent(type,name);
+		var color = Raphael.getColor();			
+		var pinConfig =getPinConfig(name);		
+		var p=pinConfig.p;
+		var b=pinConfig.b;
+		var group=[];		
+		$.each(pinConfig.pins,function(i,pos){
 			var pin = r.rect(b.x+pos.x, b.y+pos.y, p.w, p.h, p.r);
 			group.push({node:pin,p:pos});
 			pin.attr({fill: 'black', stroke: 'black', "fill-opacity": 0, "stroke-width": 2, cursor: 'crosshair' });		
@@ -117,79 +124,25 @@
 			pin.pinType=pos.pinType;
 			pin.pinId = pos.pinId;
 		});	
-		var body=r.image("img/"+gatetype+".png", b.x, b.y, b.w, b.h);
+		var body=r.image("img/"+name+".png", b.x, b.y, b.w, b.h);		
 		body.group=group;	
 		body.attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
-		body.drag(moveTo, onstart, onend);	onstart.call(body);
-		component.ui = body;	
-		component.getPin = getPin;
-		components.push(component);
-		return component;
-	};
-
-
-	var createBulb =function(){
-		var component=dlcore.getBulb();	
-		var color = Raphael.getColor();	
-		var group=[];
-		var b={x:0,y:0,w:60,h:50,r:50};
-		var p={w:20,h:10,r:0};
-		var pos={x:-p.w,y:2*b.h/5,pinType:'in'};
-		var pin = r.rect(b.x+pos.x, b.y+pos.y, p.w, p.h, p.r);
-			pin.attr({fill: 'black', stroke: 'black', "fill-opacity": 0, "stroke-width": 2, cursor: 'crosshair' });
-			pin.mouseup(mouseuponpin);		
-			pin.component=component;	
-			pin.pinType=pos.pinType;pin.pinId = 0;
-			group.push({node:pin,p:pos});
-		var body=r.rect(b.x, b.y, b.w, b.h, b.r);	
-		body.group=group;	
-		body.attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
-		body.drag(moveTo, onstart, onend);	onstart.call(body);
-		component.ui = body;	
-		component.getPin = getPin;
-		components.push(component);
-		return component;
-	};
-
-	var createSwitch = function(){
-		var component=dlcore.getSwitch();	
-		var color = Raphael.getColor();	
-		var group=[];
-		var b={x:0,y:0,w:60,h:50,r:10};
-		var p={w:20,h:10,r:0};
-		var pos={x:b.w,y:2*b.h/5,pinType:'out'};
-		var pin = r.rect(b.x+pos.x, b.y+pos.y, p.w, p.h, p.r);
-			pin.attr({fill: 'black', stroke: 'black', "fill-opacity": 0, "stroke-width": 2, cursor: 'crosshair' });
-			pin.mouseup(mouseuponpin);		
-			pin.component=component;	
-			pin.pinType=pos.pinType;pin.pinId = 0;
-			group.push({node:pin,p:pos});
-		//var body=r.rect(b.x, b.y, b.w, b.h, b.r);	
-		
-		var body=r.image("img/SWITCH_0.png", b.x, b.y, b.w, b.h);	
-		
-		body.group=group;	
-		body.attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
-		body.drag(moveTo, onstart, onend); onstart.call(body);	
-		
+		body.drag(moveTo, onstart, onend);	onstart.call(body);		
 		body.node.onclick = function(){		
-			component.toggle().state?body.attr({'src': 'img/SWITCH_1.png'}):body.attr({'src': 'img/SWITCH_0.png'});
-		};
+			component.click();
+		};				
+		component.afterUpdate=function(){				
+			component.state?body.attr({'src': 'img/'+name+'_1.png'}):body.attr({'src': 'img/'+name+'.png'});
+		};		
 		component.ui = body;	
 		component.getPin = getPin;
 		components.push(component);
 		return component;
-	};
-
-	var componentMap ={
-		GATE:createGate,
-		BULB:createBulb,
-		SWITCH:createSwitch
 	};	
 	
 	var createComponent =function(options){
 		var s=$.extend({type:'BULB', x:0, y:0}, options);		
-		var component = componentMap[s.type](s.name);
+		var component = constructComponent(s.type,s.name);
 		component.ui.settings = s;	moveTo.call(component.ui,s.x,s.y);		
 		return component;
 	};
